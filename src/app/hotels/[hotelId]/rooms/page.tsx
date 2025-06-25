a2c7c49897ff57b6eabee570e9f7ca982b1da36c
Pwinvit Boonsri <84969892+Bwinvit@users.noreply.github.com>
2025-06-25 15:35:29 +0700

    Add room deletion support

--- src/app/hotels/[hotelId]/rooms/page.tsx
+++ src/app/hotels/[hotelId]/rooms/page.tsx
@@ -1,16 +1,9 @@
 import RoomsPageClient from './RoomsPageClient'
-import { Room } from './RoomsTable'
 
 interface Params {
   params: { hotelId: string }
 }
 
 export default async function RoomsPage({ params }: Params) {
-  const res = await fetch(
-    `http://localhost:3000/api/hotels/${params.hotelId}/rooms`,
-    { cache: 'no-store' }
-  )
-  const rooms: Room[] = await res.json()
-
-  return <RoomsPageClient hotelId={params.hotelId} rooms={rooms} />
+  return <RoomsPageClient hotelId={params.hotelId} />
 }
